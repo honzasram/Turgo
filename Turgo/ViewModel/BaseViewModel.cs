19b747ced214a8530156ce5ab095be0eeed484cd
jan.sramek <jan.sramek@ingeteam.com>
2017-11-29 22:04:54 +0100

    adding PDF libs, small refactor...

---
+++ Turgo/ViewModel/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using Dragablz;
+using log4net;
+
+namespace Turgo.ViewModel
+{
+    public class BaseViewModel : HeaderedItemViewModel
+    {
+        protected static ILog mLog = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    }
+}
--- Turgo/ViewModel/PlayersViewModel.cs
+++ Turgo/ViewModel/PlayersViewModel.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection;
 using System.Windows.Input;
-using Dragablz;
-using log4net;
 using Sramek.FX.WPF;
 using Turgo.Common.Model;
 using Turgo.Controller;
@@ -39,10 +36,5 @@ namespace Turgo.ViewModel
         {
             Players = new ObservableCollection<User>(TurgoController.I.GetUserBase());
         }
-    }
-
-    public class BaseViewModel : HeaderedItemViewModel
-    {
-        protected static ILog mLog = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
     }
 }
